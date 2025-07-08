a7c37a208ce9d56944e73fe7f019733b43c2c79f
LLOKAI <172426821+LLOKAI@users.noreply.github.com>
2025-07-08 13:04:09 -0400

    file and folder structure initalized.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss";
 
-const config = {
+const config: Config = {
   darkMode: ["class"],
   content: [
     "./pages/**/*.{ts,tsx}",
@@ -104,6 +104,6 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config;
+};
 
 export default config;
