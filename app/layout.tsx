d977cc34bdd6f9497ebd6bfe3fa4ed3e52ab4737
LLOKAI <172426821+LLOKAI@users.noreply.github.com>
2025-07-10 20:08:02 -0400

    Fixed tailwindcss issues by reverting to v3 and added style to check it works

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.variable} ${ibmPlexSerif.variable}`}
+        className={`${inter.variable} ${ibmPlexSerif.variable} bg-red-500`}
       >
         {children}
       </body>
